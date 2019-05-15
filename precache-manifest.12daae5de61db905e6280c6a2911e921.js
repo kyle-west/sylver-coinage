836a168b0db10987270f0d60f3fda9014f2b3761
Kyle West <kyle@familysearch.org>
2019-05-15 09:23:03 -0600

    Updates

---
+++ precache-manifest.12daae5de61db905e6280c6a2911e921.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8748c39af69e49eb4b85bf4020b12182",
+    "url": "/sylver-coinage/index.html"
+  },
+  {
+    "revision": "825aceece827a3d8bc60",
+    "url": "/sylver-coinage/static/css/main.3729c1f6.chunk.css"
+  },
+  {
+    "revision": "9efb22fc3547bda03361",
+    "url": "/sylver-coinage/static/js/2.ebae625f.chunk.js"
+  },
+  {
+    "revision": "825aceece827a3d8bc60",
+    "url": "/sylver-coinage/static/js/main.de4e85f7.chunk.js"
+  },
+  {
+    "revision": "a27a8c87d38cd476b7d8",
+    "url": "/sylver-coinage/static/js/runtime~main.319ea5c1.js"
+  }
+]);
